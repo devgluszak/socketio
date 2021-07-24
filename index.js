709145e82fa5188d6a546c7ba80eaed034ee460e
devgluszak <devgluszak@gmail.com>
2021-07-24 23:46:27 +0200

    add lucky guy

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 4000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello Worldd!\n')
+  res.end('Hello World i em lucky man!\n')
 })
 
 server.listen(port, hostname, () => {
